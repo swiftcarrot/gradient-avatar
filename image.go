1c1d00354d6498e21df7b7a4b7ef4060139dd0ae
Wang Zuo <wzuoadjusted@gmail.com>
2018-10-23 21:42:18 +0800

    text string length fix

--- image.go
+++ image.go
@@ -4,7 +4,6 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
-	"log"
 
 	"github.com/wangzuo/color"
 )
@@ -13,17 +12,10 @@ func GenerateGradient(username, text string, width, height int) (string, error) 
 	hasher := md5.New()
 	io.WriteString(hasher, username)
 	hash := fmt.Sprintf("%x", hasher.Sum(nil))
-
 	color1 := hashStringToColor(hash)
-
-	log.Println("color1", color1)
-
 	h, s, l := color.Hex2HSL(color1)
 
-	log.Println("hsl", h, s, l)
-
 	s = s + s*0.5
-
 	if l < 25 {
 		l = l + l*3
 	} else if l > 25 && l < 40 {
@@ -32,20 +24,15 @@ func GenerateGradient(username, text string, width, height int) (string, error) 
 		l = l - l*0.4
 	}
 
-	log.Println("hsl", h, s, l)
-
 	color1 = color.HSL2Hex(h, s, l)
 	color2 := getMatchingColor(h, s, l)
-
-	log.Println(color1, color2)
-
 	avatar, err := CreateSVG(SVGData{
 		Color1:   color1,
 		Color2:   color2,
 		Width:    width,
 		Height:   height,
 		Text:     text,
-		FontSize: (float64(height) * 0.9) / float64(len(text)),
+		FontSize: (float64(height) * 0.9) / float64(len([]rune(text))),
 	})
 	if err != nil {
 		return "", err
