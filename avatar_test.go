9d9fc9efad405bea166f200123dcacbaa5809cb6
Wang Zuo <wzuoadjusted@gmail.com>
2020-12-29 19:01:39 +0800

    fix svg

--- avatar_test.go
+++ avatar_test.go
@@ -1,15 +1,12 @@
 package avatar
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestGenerateGradient(t *testing.T) {
-	avatar, err := GenerateGradient("swiftcarrot", "王", 100, 100)
+	_, err := GenerateGradient("swiftcarrot", "王", 100, 100)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	fmt.Println(avatar)
 }
--- svg.go
+++ svg.go
@@ -15,16 +15,16 @@ type SVGData struct {
 }
 
 func CreateSVG(data SVGData) (string, error) {
-	t := template.Must(template.New("svg").Parse(`<?xml version="1.0" standalone="no"?>
+	t := template.Must(template.New("svg").Parse(`<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg width="{{.Width}}" height="{{.Height}}" viewBox="0 0 {{.Width}} {{.Height}}" version="1.1" xmlns="http://www.w3.org/2000/svg">
+<svg width="{{.Width}}" height="{{.Height}}" viewBox="0 0 {{.Width}} {{.Height}}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="avatar" x1="0" y1="0" x2="1" y2="1">
+      <stop offset="0%" stop-color="{{.Color1}}"/>
+      <stop offset="100%" stop-color="{{.Color2}}"/>
+    </linearGradient>
+  </defs>
   <g>
-    <defs>
-      <linearGradient id="avatar" x1="0" y1="0" x2="1" y2="1">
-        <stop offset="0%" stop-color="{{.Color1}}"/>
-        <stop offset="100%" stop-color="{{.Color2}}"/>
-      </linearGradient>
-    </defs>
     <rect fill="url(#avatar)" x="0" y="0" width="{{.Width}}" height="{{.Height}}"/>
     {{if .Text}}<text x="50%" y="50%" alignment-baseline="central" dominant-baseline="middle" text-anchor="middle" fill="#fff" font-family="sans-serif" font-size="{{.FontSize}}">{{.Text}}</text>{{ end }}
   </g>
